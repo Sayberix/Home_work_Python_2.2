9de91b208f7419bd127b4dd4a22f9a6d180d529e
Sayberix <mr.maaks@gmail.com>
2022-11-27 23:16:13 +0700

    задача выполнена

--- home_work_2.2.py
+++ home_work_2.2.py
@@ -7,3 +7,22 @@
 # --> 0 2 3
 # -3 * -1 * 0 = 0
 # Вывод: 0
+
+n = int(input('Введите целое число: '))
+listOfNumber = list()
+countList = 0
+for i in range(-n,n+1):
+    listOfNumber.append(i)
+    countList += 1
+print(listOfNumber, 'количество элементов в списке: ', countList)
+seq = input('Введите индексы элементов через пробел, произведение которых необходимо найти: ')
+multiplication = 1
+for i in range(len(seq)):
+    if seq[i].isdigit():
+        if int(seq[i]) < countList:
+            multiplication *= listOfNumber[int(seq[i])]
+        else:
+            print('Ошибка в вводе. Введенный элемент вне диапазона списка. Программа завершает работу!')
+            break
+    if i == len(seq)-1:
+        print('Произведение выбранных элементов: ', multiplication)
