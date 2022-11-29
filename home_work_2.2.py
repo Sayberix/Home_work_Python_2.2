83fabd08bf75919304bf5bc411a50e7f2830bb2f
Sayberix <mr.maaks@gmail.com>
2022-11-30 00:05:39 +0700

    задача выполнена другим способом с использованием random.choise

--- home_work_2.2.py
+++ home_work_2.2.py
@@ -3,10 +3,18 @@
 import random
 
 number = list()
-print('инициализированный список - ',number)
+number2 = list()
 n = 10
 for i in range(n):
     number.append(i)
 print('отсортированный список - ',number)
-random.shuffle(number)
-print('перемешанный список - ',number)
+indexList = len(number)
+for i in range(indexList,0,-1):
+    if indexList > 0:
+       element = random.choice(range(i))
+       number2.append(number[element])
+       number.pop(int(element))
+    else:
+        number2.append(number[0])
+        break
+print('перемешанный список - ',number2)
