790212647d05299ab5c813e9e7f579a700f5c112
Sayberix <mr.maaks@gmail.com>
2022-11-29 21:13:32 +0700

    задача выполнена простым способом - встроенной функцией shuffle

--- home_work_2.2.py
+++ home_work_2.2.py
@@ -1,1 +1,12 @@
 # 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+
+number = list()
+print('инициализированный список - ',number)
+n = 10
+for i in range(n):
+    number.append(i)
+print('отсортированный список - ',number)
+random.shuffle(number)
+print('перемешанный список - ',number)
